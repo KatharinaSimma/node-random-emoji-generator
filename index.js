008e4ef66a01641834682f39b4bf6ab52d910df2
Katharina Simma <katharina.simma@diebin.at>
2023-01-10 12:47:40 +0100

    Add basic logic of the program

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log('Hello Word!');
+import emoji from 'node-emoji';
 
-c();
+const userEmoji = process.argv[2];
+let result;
+
+if (!userEmoji) {
+  result = emoji.random().emoji;
+} else if (emoji.hasEmoji(userEmoji)) {
+  result = emoji.get(userEmoji);
+} else {
+  result = `${userEmoji} is not a valid emoji. Try again!`;
+}
+
+console.log(result);
